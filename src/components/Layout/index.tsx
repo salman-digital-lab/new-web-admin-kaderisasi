b6e2fa5a91f8858befb8f9e711ae9b30dbf5324d
dwiana161 <dwianakas@gmail.com>
2024-01-06 12:54:19 +0700

    feat : initiate admin kaderisasi layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
+import { Layout, Button, theme } from 'antd';
+import SideMenu from '../SideMenu';
+
+const { Header, Content } = Layout;
+
+type ContentProps= {
+  children : React.ReactNode
+}
+
+const AppLayout: React.FC<ContentProps> = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const handleCollapse = (collapsed: boolean) => {
+    setCollapsed(collapsed);
+  };
+
+  return (
+     <Layout>
+      <SideMenu collapsed={collapsed} onCollapse={handleCollapse}/>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            height: '100vh',
+            background: colorBgContainer,
+          }}
+        >
+          {children}
+        </Content>
+      </Layout>
+    </Layout>
+  )
+}
+
+export default AppLayout
