baeec7fa45bf29b5805c6580af3ee1b74bb2ffc2
dwiana161 <dwianakas@gmail.com>
2024-02-05 05:31:10 +0700

    feat : add universities modal page

--- src/pages/MainUniversities/index.tsx
+++ src/pages/MainUniversities/index.tsx
@@ -1,53 +1,53 @@
 import React, { useState } from 'react';
 import UniversitiesTable from './components/UniversitiesTable';
 import UniversitiesFilter from './components/UniversitiesFilter';
-import { DataUniversities } from '../../types';
+import { DataMaster } from '../../types';
 
-const userData: DataUniversities[] = [
+const userData: DataMaster[] = [
     {
         key: '1',
         no : 1,
-        university: 'STIKES Cianjur'
+        name: 'STIKES Cianjur'
       },
       {
         key: '2',
         no : 2,
-        university: 'International Open University',
+        name: 'International Open University',
       },
       {
         key: '3',
         no : 3,
-        university: 'Institut Teknologi Telkom Surabaya',
+        name: 'Institut Teknologi Telkom Surabaya',
       },
       {
         key: '4',
         no : 4,
-        university: 'Universitas Wanita Internasional',
+        name: 'Universitas Wanita Internasional',
       },
       {
         key: '5',
         no : 5,
-        university: 'STAI Muttaqin Purwakarta',
+        name: 'STAI Muttaqin Purwakarta',
       },
       {
         key: '6',
         no : 6,
-        university: 'STAI al Muhajirin Purwakarta',
+        name: 'STAI al Muhajirin Purwakarta',
       },
       {
         key: '7',
         no : 7,
-        university: 'Universitas Buana Perjuangan Karawang',
+        name: 'Universitas Buana Perjuangan Karawang',
       },
   ];
 
 const MainUniversities: React.FC = () => {
 
-  const [filteredData, setFilteredData] = useState<DataUniversities[]>(userData);
+  const [filteredData, setFilteredData] = useState<DataMaster[]>(userData);
 
   const handleSearch = (searchValue: string) => {
     const newData = userData.filter((item) =>
-      item.university.toLowerCase().includes(searchValue.toLowerCase())
+      item.name.toLowerCase().includes(searchValue.toLowerCase())
     );
     setFilteredData(newData);
   };
