788aff4ab6d69fa274319676606cef54b3f1d987
dwiana161 <dwianakas@gmail.com>
2024-02-05 05:24:38 +0700

    feat : add logo and logout button

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,15 +3,47 @@ import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
 } from '@ant-design/icons';
-import { Layout, Button, theme } from 'antd';
+import { 
+  Layout, 
+  Button, 
+  theme, 
+  Typography, 
+  Row, 
+  Col, 
+  Avatar, 
+  Dropdown, 
+  Space, 
+  MenuProps,
+  message 
+} from 'antd';
 import SideMenu from '../SideMenu';
 import { Outlet } from 'react-router-dom';
+import { UserOutlined, DownOutlined } from '@ant-design/icons';
 
 const { Header, Content } = Layout;
+const { Text } = Typography;
 
 // type ContentProps = {
 //   children : React.ReactNode
 // }
+
+const handleMenuClick: MenuProps['onClick'] = (e) => {
+  message.info('Click on menu item.');
+  console.log('click', e);
+};
+
+const items: MenuProps['items'] = [
+  {
+    label: 'Logout',
+    key: '1',
+    icon: <UserOutlined />,
+  }
+];
+
+const menuProps = {
+  items,
+  onClick: handleMenuClick,
+};
 
 const AppLayout: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -28,16 +60,31 @@ const AppLayout: React.FC = () => {
       <SideMenu collapsed={collapsed} onCollapse={handleCollapse}/>
       <Layout>
         <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
+          <Row justify="space-between">
+            <Col>
+              <Button
+                type="text"
+                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                onClick={() => setCollapsed(!collapsed)}
+                style={{
+                  fontSize: '16px',
+                  width: 64,
+                  height: 64,
+                }}
+              />
+            </Col>
+            <Col>
+              <Dropdown menu={menuProps}>
+                <Button style={{height:'5vh', width:'auto', marginRight:'20px', borderRadius: '20px'}}>
+                  <Space>
+                    <Avatar size={25} style={{ backgroundColor: '#87d068', marginRight:'5px' }} icon={<UserOutlined />} />
+                    <Text style={{ textAlign: 'right', marginRight:'5px'}}>Hello, administrator!</Text>
+                    <DownOutlined style={{marginRight:'10px'}}/>
+                  </Space>
+                </Button>
+              </Dropdown>
+            </Col>
+          </Row>
         </Header>
         <Content
           style={{
