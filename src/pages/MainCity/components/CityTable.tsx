161200d5d0230fdd124fbd1cec1ef6613fe7e828
dwiana161 <dwianakas@gmail.com>
2024-02-05 05:29:33 +0700

    feat : add city page

---
+++ src/pages/MainCity/components/CityTable.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Button, Space, Table, TableProps, Modal, Input, Form, Select } from 'antd';
+import { DataMaster } from '../../../types';
+import { EditOutlined } from '@ant-design/icons';
+
+interface DataTypeProps {
+  data : DataMaster[];
+}
+
+const { Option } = Select;
+
+const CityTable: React.FC<DataTypeProps>  = ({ data }) => {
+  const [dataEdit, setDataEdit] = useState<boolean>(false);
+
+    const pagination = {
+        pageSize: 5,
+        showSizeChanger: true,
+      };
+
+  const columns : TableProps<DataMaster>['columns'] = [
+    {
+      title: 'No',
+      dataIndex: 'no',
+      key: 'no',
+    },
+    {
+      title: 'Nama Provinsi',
+      dataIndex: 'province_id',
+      key: 'province_id',
+    },
+    {
+      title: 'Nama Kota',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <Space size="middle">
+            <Button
+                type='primary'
+                size='large'
+                shape='round'
+                icon={ <EditOutlined /> }
+                style={{ backgroundColor:'teal' }}
+                onClick={() => setDataEdit(true)}
+            > Edit </Button>
+            <Modal
+            title="Edit Kabupaten/Kota"
+            centered
+            width={'40vw'}
+            mask={false}
+            open={dataEdit}
+            onOk={() => setDataEdit(false)}
+            onCancel={() => setDataEdit(false)}
+          >
+            <Form layout='vertical'>
+              <Form.Item label="Provinsi">
+                <Select placeholder="Nama Provinsi" defaultValue="Jawa Barat">
+                    <Option value="1">Jawa Barat</Option>
+                    <Option value="2">Banten</Option>
+                    <Option value="3">Aceh</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item label="Kabupaten/Kota">
+                <Input defaultValue="Bandung" size='large'/>
+              </Form.Item>
+            </Form>
+          </Modal>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div>
+      <Table 
+        columns={columns} 
+        dataSource={data}
+        pagination={pagination} 
+       />
+    </div>
+  )
+}
+
+export default CityTable;
