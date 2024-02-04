de4d13c76c483d4e1a63b1994e31661f0a5cfe44
dwiana161 <dwianakas@gmail.com>
2024-02-05 05:30:28 +0700

    feat : add province page

---
+++ src/pages/MainProvince/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import ProvinceTable from './components/ProvinceTable';
+import ProvinceFilter from './components/ProvinceFilter';
+import { DataMaster } from '../../types';
+
+const userData: DataMaster[] = [
+    {
+        key: '1',
+        no : 1,
+        name: 'Jawa Barat'
+      },
+      {
+        key: '2',
+        no : 2,
+        name: 'Jawa Timur',
+      },
+      {
+        key: '3',
+        no : 3,
+        name: 'Bali',
+      },
+      {
+        key: '4',
+        no : 4,
+        name: 'Aceh',
+      },
+      {
+        key: '5',
+        no : 5,
+        name: 'Banten',
+      },
+      {
+        key: '6',
+        no : 6,
+        name: 'Sumatera Utara',
+      },
+      {
+        key: '7',
+        no : 7,
+        name: 'Daerah Istimewa Yogyakarta',
+      },
+  ];
+
+const MainProvince: React.FC = () => {
+
+  const [filteredData, setFilteredData] = useState<DataMaster[]>(userData);
+
+  const handleSearch = (searchValue: string) => {
+    const newData = userData.filter((item) =>
+      item.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(newData);
+  };
+
+  return (
+    <div>
+      <ProvinceFilter onSearch={handleSearch}/>
+      <ProvinceTable data={filteredData} />
+    </div>
+  );
+};
+
+export default MainProvince;
