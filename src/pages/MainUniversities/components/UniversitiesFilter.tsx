a5db933a3662958b81387f902902e877d8cd3472
dwiana161 <dwianakas@gmail.com>
2024-02-05 05:31:10 +0700

    feat : add universities modal page

--- src/pages/MainUniversities/components/UniversitiesFilter.tsx
+++ src/pages/MainUniversities/components/UniversitiesFilter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Input, Flex, Col, Row, Button } from 'antd';
+import { Input, Flex, Col, Row, Button, Modal } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { PlusCircleFilled } from '@ant-design/icons';
 
@@ -9,6 +9,7 @@ interface FilterProps{
 
 const UniversitiesFilter: React.FC<FilterProps>= ({ onSearch }) => {
   const [searchText, setSearchText] = useState<string>('');
+  const [dataAdd, setDataAdd] = useState<boolean>(false); 
 
   const handleSearch = () => {
     onSearch(searchText);
@@ -20,7 +21,7 @@ const UniversitiesFilter: React.FC<FilterProps>= ({ onSearch }) => {
         <Col className="gutter-row" span={18}>
             <Input 
               size="large"
-              placeholder="input search text" 
+              placeholder="nama universitas" 
               prefix={<SearchOutlined />}
               value={searchText}
               onChange={(e) => setSearchText(e.target.value)}
@@ -28,9 +29,26 @@ const UniversitiesFilter: React.FC<FilterProps>= ({ onSearch }) => {
             />
         </Col>
         <Col className="gutter-row" span={6}>
-            <Button type='primary' size='large' icon={<PlusCircleFilled />} block>
+            <Button 
+                type='primary' 
+                size='large' 
+                icon={<PlusCircleFilled />} 
+                block
+                onClick={() => setDataAdd(true)}
+              >
                Universitas
             </Button>
+            <Modal
+            title="Tambah Universitas"
+            centered
+            width={'40vw'}
+            mask={false}
+            open={dataAdd}
+            onOk={() => setDataAdd(false)}
+            onCancel={() => setDataAdd(false)}
+          >
+            <Input placeholder="Nama Universitas*" size='large' style={{margin:'20px 0px 20px'}}/>
+          </Modal>
         </Col>
       </Row>
   </Flex>
