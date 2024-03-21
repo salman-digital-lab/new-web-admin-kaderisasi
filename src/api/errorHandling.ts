0f9c2ee52f9102cfc1d4775cfed1b02f11d60692
Naufal Zhafran Latif <latifnaufal7@gmail.com>
2024-03-21 13:26:37 +0700

    feat: add error handling for 401 error

--- src/api/errorHandling.ts
+++ src/api/errorHandling.ts
@@ -2,19 +2,31 @@ import { notification } from "antd";
 import { AxiosError } from "axios";
 
 export const handleError = (error: unknown) => {
-  if (error instanceof Error) {
-    const errorMessage = error.message;
-    notification.error({
-      message: "Gagal",
-      description: `Terjadi kegagalan: ${errorMessage}`,
-    });
-  } else if (isAxiosError(error)) {
+  if (isAxiosError(error)) {
     const axiosError = error as AxiosError;
     if (axiosError.response) {
-      notification.error({
-        message: "Gagal",
-        description: `Server membalas dengan: ${axiosError.response.data}. Kode error: ${axiosError.response.status}`,
-      });
+      if (axiosError.response.status === 401) {
+        notification.error({
+          message: "Gagal",
+          description: `Anda tidak memiliki akses`,
+        });
+
+        window?.location?.replace(window.location.origin + "/login");
+      } else if (
+        axiosError.response.data &&
+        typeof axiosError.response.data === "object" &&
+        "message" in axiosError.response.data
+      ) {
+        notification.error({
+          message: "Gagal",
+          description: `Server membalas dengan: ${axiosError.response.data.message}. Kode error: ${axiosError.response.status}`,
+        });
+      } else {
+        notification.error({
+          message: "Gagal",
+          description: `Server membalas dengan: ${axiosError.message}. Kode error: ${axiosError.response.status}`,
+        });
+      }
     } else if (axiosError.request) {
       notification.error({
         message: "Gagal",
@@ -28,6 +40,12 @@ export const handleError = (error: unknown) => {
           "Telah terjadi kegagalan, silahkan ulangi setelah beberapa saat",
       });
     }
+  } else if (error instanceof Error) {
+    const errorMessage = error.message;
+    notification.error({
+      message: "Gagal",
+      description: `Terjadi kegagalan: ${errorMessage}`,
+    });
   } else {
     console.error("Unknown error:", error);
     notification.error({
